e64b0e2005148cd23cb2b4e03adc097ffbb3a1fd
suraj chauhan <sonubintw@gmail.com>
2023-06-19 11:52:43 +0530

    " cors origin update"

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use(cors({
-    origin: ["http://localhost:3000", "https://inventory-for-shop.vercel.app"],
+    origin: ["http://localhost:3000", "https://inventory-for-shop.vercel.app", "https://front-inventory.onrender.com"],
     // origin: true,//origin :"*" not working
     credentials: true,
 
